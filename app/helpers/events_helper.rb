9918c99369018aed25b03ac992493a7294fd9a13
chouda <yujiankea@gmail.com>
2017-08-16 15:53:44 -0700

    over

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,13 @@
 module EventsHelper
+
+  def display_event_status(event)
+     case event.status
+       when "draft"
+         "草稿"
+       else
+         ""
+     end
+   end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410085333) do
+ActiveRecord::Schema.define(version: 20170608054313) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.string   "friendly_id"
+    t.string   "status",      default: "draft"
+    t.index ["friendly_id"], name: "index_events_on_friendly_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,6 +36,7 @@ ActiveRecord::Schema.define(version: 20170410085333) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "time_zone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
